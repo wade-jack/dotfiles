423223b57900bb9a1dba078e95747972da87f4fc
Jack Wade <jackwade@egtechnology.co.uk>
2026-06-01 09:56:57 +0100

    Update configuration

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,18 +1,41 @@
-/opt/homebrew/bin/brew shellenv | source
-set -gx MICRO_TRUECOLOR 1
+if test (uname) = Darwin
+    if test -x /opt/homebrew/bin/brew
+        eval (/opt/homebrew/bin/brew shellenv)
+    else if test -x /usr/local/bin/brew
+        eval (/usr/local/bin/brew shellenv)
+    end
+else if test -x /home/linuxbrew/.linuxbrew/bin/brew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+else if test -x /home/linuxbrew/.homebrew/bin/brew
+    eval (/home/linuxbrew/.homebrew/bin/brew shellenv)
+end
+
+for completion_dir in \
+   /opt/homebrew/share/fish/vendor_completions.d \
+   /usr/local/share/fish/vendor_completions.d \
+   /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d \
+   /home/linuxbrew/.homebrew/share/fish/vendor_completions.d
+   if test -d "$completion_dir"
+       if not contains -- "$completion_dir" $fish_complete_path
+           set -g fish_complete_path "$completion_dir" $fish_complete_path
+       end
+   end
+end
 
 if status is-interactive
-    fnm env --use-on-cd --shell fish | source
-    starship init fish | source
-    fzf --fish | source
-    zoxide init fish | source
-    if command -q thefuck
-        env TF_SHELL=fish thefuck --alias | source
+   if command -q fnm
+       fnm env --use-on-cd --shell fish | source
+   end
+    if command -q fzf
+        fzf --fish | source
+    end
+    if command -q zoxide
+        zoxide init fish | source
     end
 end
 
 function y --wraps yazi --description "Yazi wrapper that cds into the last visited directory"
-    set -l tmp (mktemp -t yazi-cwd.XXXXXX)
+    set -l tmp (mktemp)
     command yazi $argv --cwd-file="$tmp"
 
     if test -f "$tmp"
